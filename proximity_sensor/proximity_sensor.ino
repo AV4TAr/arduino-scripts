56cbd3b90a4ef2b3f42cf364b450a9df1dee057b
Diego Sapriza <AV4TAr@gmail.com>
2015-11-05 23:19:51 -0300

    first commit

---
+++ proximity_sensor/proximity_sensor.ino
@@ -0,0 +1,59 @@
+/*
+HC-SR04 Ping distance sensor]
+VCC to arduino 5v GND to arduino GND
+Echo to Arduino pin 13 Trig to Arduino pin 12
+Red POS to Arduino pin 11
+Green POS to Arduino pin 10
+560 ohm resistor to both LED NEG and GRD power rail
+More info at: http://www.trollmaker.com/article3/arduino-and-hc-sr04-ultrasonic-sensor
+Original code improvements to the Ping sketch sourced from Trollmaker.com
+Some code and wiring inspired by http://en.wikiversity.org/wiki/User:Dstaub/robotcar
+*/
+
+#define trigPin 13
+#define echoPin 12
+
+#define redLed 11
+#define greenLed 10
+
+#define minDistance 4
+#define maxDistance 100
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+}
+
+void printDistance(int distance){
+  Serial.print(distance);
+  Serial.println(" cm");
+}
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2); 
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  //The Pace of Sound is approximately 29.1 microseconds per centimetre. 
+  distance = (duration/2) / 29.1;
+  
+  if (distance < minDistance) {
+    digitalWrite(redLed,HIGH); 
+    digitalWrite(greenLed,LOW);
+  } else {
+    digitalWrite(redLed,LOW);
+    digitalWrite(greenLed,HIGH);
+  }
+  
+  if (distance >= maxDistance || distance <= 0){
+    Serial.println("Out of range");
+  } 
+    printDistance(distance);
+  
+  delay(500);
+}
